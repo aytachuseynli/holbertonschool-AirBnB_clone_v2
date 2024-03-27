23b50410963c3554862d3742354b6f3abaeb13f4
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-28 01:16:57 +0400

    task 6 updated

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
 
 Base = declarative_base()
@@ -10,26 +11,27 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+        id = Column(String(60), nullable=False, primary_key=True, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        from models import storage
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if key in ("created_at", "updated_at"):
+                    value = datetime.strptime(kwargs['updated_at'],
+                                            '%Y-%m-%dT%H:%M:%S.%f')
+                if "__class__" not in key:
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = type(self).__name__
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
@@ -41,9 +43,14 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = self.__dict__.copy()
-        dictionary.pop('_sa_instance_state', None)
-        dictionary['__class__'] = type(self).__name__
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        try:
+            del dictionary['_sa_instance_state']
+        except Exception:
+            pass
+        dictionary.update({'__class__':
+                               (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
--- models/state.py
+++ models/state.py
@@ -11,6 +11,9 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
+    def __init__(self, *args, **kwargs):
+        """init inherited"""
+        super().__init__(*args, **kwargs)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
