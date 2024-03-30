419a23c5ab58d2c0239991ab67db3e3a98b67ba7
aytachuseynli <aytac_ha@mail.ru>
2024-03-31 01:00:36 +0400

    Updated States

--- models/state.py
+++ models/state.py
@@ -11,6 +11,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
+
     def __init__(self, *args, **kwargs):
         """init inherited"""
         super().__init__(*args, **kwargs)
