6e27588b83f9fb03c8adad96319e2e4e1342472a
aytachuseynli <aytac_ha@mail.ru>
2024-03-30 22:21:52 +0000

    place modify

--- models/place.py
+++ models/place.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
+
 
 class Place(BaseModel):
     """ A place to stay """
