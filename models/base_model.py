963706fad45c86968cbc6e034b35fe54542cf7af
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-30 23:44:33 +0400

    again

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = (str(type(self)).split('.')[-1]).split("'")[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
