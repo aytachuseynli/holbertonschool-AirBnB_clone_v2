5754e90c5d63bc93d58b91a30c1605c50668c6fa
arzumirzeb <arzumirzeb03@gmail.com>
2024-04-02 19:21:44 +0400

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModeli, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nulable=False)
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                   overlaps="amenities", viewonly=False)
--- models/place.py
+++ models/place.py
@@ -21,7 +21,14 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
     reviews = relationship('Review', cascade='all, delete', backref='place')
-
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
+    amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def reviews(self):
@@ -33,4 +40,20 @@ class Place(BaseModel, Base):
                 if rev.place_id == self.id:
                     result.append(rev)
             return result
-    
+        
+        @property
+        def amenities(self):
+            """getter Function"""
+            from models import storage
+            result = []
+            all_amenities = storage.all(Amenity).values()
+            for amenity in all_amenities:
+                if amenity.id in self.amenity_ids:
+                    result.append(amenity)
+            return result
+
+        @amenities.setter
+        def amenities(self, obj):
+            """setter Function"""
+            if type(obj) is Amenity:
+                self.amenity_ids.append(obj.id)
