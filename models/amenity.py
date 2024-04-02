78df80a6550a700044bb780df5daf9c26ea80667
Arzumirzeb <arzumirzeb03@gmail.com>
2024-04-02 21:01:23 +0400

    hotfix

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModeli, Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    name = Column(String(128), nulable=False)
+    name = Column(String(128), nullable=False)
     place_amenities = relationship('Place', secondary='place_amenity',
                                    overlaps="amenities", viewonly=False)
