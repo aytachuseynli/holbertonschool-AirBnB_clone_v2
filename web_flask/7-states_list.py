00cda74b209f83ecd68af2e3ad4418fe261e8dc7
aytachuseynli <aytac_ha@mail.ru>
2024-04-21 02:27:50 +0400

    ADDED files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 
 app = Flask(__name__)
 
