a32215e9eff2de484d5eef240ff9af72cdf0e70e
aytachuseynli <aytac_ha@mail.ru>
2024-04-20 22:10:04 +0400

    updated

---
+++ web_flask/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Init file for the models module"""
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
