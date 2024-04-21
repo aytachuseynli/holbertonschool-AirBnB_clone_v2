d8a22101c1e55b9592839312543338fa4eee8d43
aytachuseynli <aytac_ha@mail.ru>
2024-04-21 22:16:42 +0400

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""This script starts a Flask web application"""
-
-from flask import Flask, render_template
+"""
+This script starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
