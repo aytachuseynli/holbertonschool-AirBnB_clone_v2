003db53504d335a887ab9fe281452a189d5df1d1
aytachuseynli <aytac_ha@mail.ru>
2024-04-21 22:08:44 +0400

    state modify

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+from models import storage
 
 
 class State(BaseModel, Base):
